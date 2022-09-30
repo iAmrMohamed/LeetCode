e60836f6395aaabd5bae998811dbf14224e6f110
Amr Mohamed <iAmrMohamed@gmail.com>
2022-09-30 14:45:09 +0200

    Time: 72 ms (93.25%), Space: 14.7 MB (17.58%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.swift
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.swift
@@ -1,17 +1,17 @@
 class Solution {
     func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
         let result = (nums1 + nums2).map { Double($0) }.sorted()
-    let count = result.count
-    
-        let output: Double
-        if count % 2 == 0 {
-            let index1 = count/2
-            let index2 = index1 - 1
-            output = (result[index1] + result[index2]) / 2
-        } else {
-            output = result[abs(count/2)]
-        }
+        let count = result.count
 
-        return output
-    }
+            let output: Double
+            if count % 2 == 0 {
+                let index1 = count/2
+                let index2 = index1 - 1
+                output = (result[index1] + result[index2]) / 2
+            } else {
+                output = result[abs(count/2)]
+            }
+
+            return output
+        }
 }
