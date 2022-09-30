5b8aea2fedde3c5539e31ed0a18bb36b91f47298
Amr Mohamed <iAmrMohamed@gmail.com>
2022-09-30 14:45:56 +0200

    Time: 80 ms (83.66%), Space: 14.6 MB (33.93%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.swift
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.swift
@@ -3,15 +3,15 @@ class Solution {
         let result = (nums1 + nums2).map { Double($0) }.sorted()
         let count = result.count
 
-            let output: Double
-            if count % 2 == 0 {
-                let index1 = count/2
-                let index2 = index1 - 1
-                output = (result[index1] + result[index2]) / 2
-            } else {
-                output = result[abs(count/2)]
-            }
-
-            return output
+        let output: Double
+        if count % 2 == 0 {
+            let index1 = count/2
+            let index2 = index1 - 1
+            output = (result[index1] + result[index2]) / 2
+        } else {
+            output = result[abs(count/2)]
         }
+
+        return output
+    }
 }
