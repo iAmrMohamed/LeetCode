d2f1d2743709b219ea4511640008d7d6c761f916
Amr Mohamed <iAmrMohamed@gmail.com>
2023-05-05 15:58:33 +0300

    Time: 0 ms, Memory: 13.8 MB - LeetHub

---
+++ guess-number-higher-or-lower/guess-number-higher-or-lower.swift
@@ -0,0 +1,32 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num -> your guess number
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0 
+ * func guess(_ num: Int) -> Int 
+ */
+
+class Solution : GuessGame {
+    func guessNumber(_ n: Int) -> Int {
+        var left = 0
+        var right = n
+        
+        while left <= right {
+            let mid = (left + right) / 2
+            let result = guess(mid)
+            
+            print("mid", mid, "result", result, "left", left, "right", right)
+            
+            if result == 0 {
+                return mid
+            } else if result == 1 {
+                left = mid + 1
+            } else if result == -1 {
+                right = mid - 1
+            }
+        }
+        
+        return right
+    }
+}
